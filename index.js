c66b8ab9160f6992b4c9197f0cbe79a393688bd0
cab driver <cocktailsthecomic@gmail.com>
2017-09-07 21:52:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function iLoveTheBeatles(number) {
 var array = [];
 var i = 0;
 do{
-  array.push("I love the Beatles!"");
+  array.push("I love the Beatles!");
   i++;}
 while (i<= number && number <=15);
 return array
