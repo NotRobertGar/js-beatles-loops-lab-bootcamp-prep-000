00af961e7b02b420ad93662b3317afbc74a1d422
cab driver <cocktailsthecomic@gmail.com>
2017-09-07 21:52:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ return array
 
 }
 
+function iLoveTheBeatles(number) {
+var array = [];
+var i = 0;
+do{
+  array.push("I love the Beatles!"");
+  i++;}
+while (i<= number && number <=15);
+return array
+}
 
 //*+ Create a function `iLoveTheBeatles` which accepts a number as a parameter.
 //The body of the function should create a variable that stores an empty array. Then, implement a do-while loop inside the function that adds
